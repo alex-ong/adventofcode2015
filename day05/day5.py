e1a8afe94091affe9b3eeb3c74ada7e1284a89c7
alexo <the.onga@gmail.com>
2023-12-10 21:34:14 +1100

    day05: part2

--- day05/day5.py
+++ day05/day5.py
@@ -1,12 +1,9 @@
-"""day5 solution"""
-
 """
-A nice string is one with all of the following properties:
-
-It contains at least three vowels (aeiou only), like aei, xazegov, or aeiouaeiouaeiou.
-It contains at least one letter that appears twice in a row, like xx, abcdde (dd), or aabbccdd (aa, bb, cc, or dd).
-It does not contain the strings ab, cd, pq, or xy, even if they are part of one of the other requirements.
+day5 solution
+https://adventofcode.com/2015/day/5
 """
+
+
 VOWELS = "aeiou"
 NAUGHTY_PAIRS = ["ab", "cd", "pq", "xy"]
 
@@ -16,7 +13,17 @@ def count_vowels(string: str):
     return sum(1 if letter in VOWELS else 0 for letter in string)
 
 
+def one_letter_gap(string: str):
+    """returns true if there's a substring like x_x"""
+    for index in range(len(string) - 2):
+        substring = string[index : index + 3]
+        if substring[0] == substring[2]:
+            return True
+    return False
+
+
 def is_nice(string: str):
+    """part1"""
     vowel_count = count_vowels(string)
 
     if vowel_count < 3:
@@ -33,6 +40,21 @@ def is_nice(string: str):
     return has_pair
 
 
+def is_nice2(string: str):
+    """part 2"""
+    if not one_letter_gap(string):
+        return False
+
+    for index in range(len(string) - 1):
+        sub_string = string[index : index + 2]
+        start_of_string = string[:index]
+        end_of_string = string[index + 2 :]
+
+        if sub_string in start_of_string or sub_string in end_of_string:
+            return True
+    return False
+
+
 def parse_input():
     """gets the lines"""
     with open("input.txt", "r", encoding="utf8") as file:
@@ -40,10 +62,14 @@ def parse_input():
 
 
 def main():
-    lines = parse_input()
+    lines: list[str] = parse_input()
 
     # q1
     nice_lines = sum(1 if is_nice(line) else 0 for line in lines)
+    print(nice_lines)
+
+    # q2
+    nice_lines = sum(1 if is_nice2(line) else 0 for line in lines)
     print(nice_lines)
 
 
